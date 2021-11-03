8c3b24400b5ca4552717466bf0635621787dd84d
AditShah1234 <ashah9497@gmail.com>
2021-11-03 05:36:50 +0530

    Used Fast API

--- app.py
+++ app.py
@@ -1,28 +1,76 @@
-from flask import Flask, flash, render_template, redirect, request, url_for
+from fastapi.encoders import jsonable_encoder
+import uvicorn
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+from starlette.responses import RedirectResponse
+import math
+app = FastAPI()
 
-app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+global X,Y ,change
+change = False
+
+X=0
+Y=0
+
+@app.get('/', response_class=HTMLResponse)
+def signin(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post('/', response_class=HTMLResponse)
+async def signin(request: Request, uname: str = Form(...), psw: str = Form(...)):
+    print(f'username: {uname}') 
+    print(f'password: {psw}')
+    name = str(uname)
+    password = str(psw)
+    get_name = name.lower()
+    print(get_name)
+    if get_name == "adit" or password=="123":
+        
+        return RedirectResponse(app.url_path_for('check_cord',**{"Name": name}))
+    else:
+        return templates.TemplateResponse("login.html", {"request": request})
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
-@app.route('/inputs', methods=['GET', 'POST'])
-def inputs():
-    error = None
-    if request.method == 'POST':
-        if request.form['X_axis'] != '0' or \
-                request.form['Y_axis'] != '0':
-            error = 'Bike is relocated!!'
-            flash('Bike is relocated!!')
-        else:
-            error = 'Bike is at rest.'
-            flash('Bike is at rest.')
-            return redirect(url_for('index'))
-    return render_template('inputs.html', error=error)
+@app.get('/check_cord/{Name}/', response_class=HTMLResponse)
+def check_cord(request: Request, Name: str):
+    
+    return templates.TemplateResponse("Co-ordinates.html", {"request": request, "Name": Name})
+
+@app.post('/check_cord/{Name}/', response_class=HTMLResponse)
+async def check_cord(request: Request,  Name : str):
+    global X
+    global Y
+  
+    html = str('https://maps.google.com/maps?q='+str(X)+","+str(Y))
+    return templates.TemplateResponse("Co-ordinates.html", {"request": request, "Name": Name, "X" : X, "Y" : Y,"HTML": html})
 
 
-if __name__ == '__main__':
-    app.run()
+
+@app.get("/co-ordinates-input")
+def input(request: Request, Name: str, Latitude: float, Longitude: float):
+    global X , Y ,change
+    
+    if math.sqrt((X-Latitude)**2 + (Y-Longitude)**2)> 10:
+        change=True
+
+     
+    print(change)
+    json_compatible_item_data = jsonable_encoder({"X":X, "Y":Y,"X_prev":Latitude, "Y_prev": Longitude, "Aleart": change})
+    Y = Longitude
+    X = Latitude
+    change =False
+    return JSONResponse(content=json_compatible_item_data)
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app",debug=True, port = 8000)
--- test.py
+++ test.py
@@ -1,52 +1,42 @@
-import unittest
-
-from app import app
-
-
-class TestFlash(unittest.TestCase):
-
-    # Ensure the index was setup correctly
-    def test_index(self):
-        tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-
-    # Ensure the inputs was setup correctly
-    def test_inputs(self):
-        tester = app.test_client(self)
-        response = tester.get('/inputs', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-
-    # Ensure the inputs page responds correctly
-    def test_inputs_response(self):
-        tester = app.test_client(self)
-        response = tester.get('/inputs', content_type='html/text')
-        self.assertTrue(b'Coordinates' in response.data)
-
-    # Ensure the correct response if the coordinates are at rest
-    def test_coordinates_rest(self):
-        tester = app.test_client(self)
-        # res = tester.post('/inputs/', data={'X_axis': '0', 'Y_axis': '0'}, follow_redirects=True)
-        res = tester.post('/inputs', data=dict(X_axis='0', Y_axis='0'), follow_redirects=True)
-        # self.assertTrue(res.text('Bike is at rest.', res.get_data(as_text=True)))
-        try:
-            assert b'Bike is at rest.' in res.data
-            print(res.data)
-        except AssertionError:
-            print(res.data)
-            print("Assertion failed!")
-
-    # Ensure the correct response if the coordinates are at rest
-    def test_coordinates_move(self):
-        tester = app.test_client(self)
-        res = tester.post('/inputs', data=dict(X_axis='0', Y_axis='0'), follow_redirects=True)
-        try:
-            assert b'Bike is relocated!!' in res.data
-            print(res.data)
-        except AssertionError:
-            print(res.data)
-            print("Assertion failed!")
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+import requests
+app = FastAPI()
 
 
+
+
+class unittest:
+    def test_read_main(self):
+        response = requests.get("http://127.0.0.1:8000/")
+     
+
+        assert response.status_code == 200
+    
+    def test_read_co_ordinates(self):
+        response = requests.get("http://127.0.0.1:8000/check_cord/231/")
+      
+    
+    def test_api(self):
+       
+        response = requests.get("http://127.0.0.1:8000/co-ordinates-input?Name=das&X_axis=32&Y_axis=23")
+      
+        assert response.status_code == 200 
+    
+    def test_api_fun(self):
+        
+            response = requests.get("http://127.0.0.1:8000/co-ordinates-input?Name=das&X_axis=0&Y_axis=0")
+            
+            assert response.json()["Aleart"] == True
+            response = requests.get("http://127.0.0.1:8000/co-ordinates-input?Name=das&X_axis=5&Y_axis=4")
+            
+            assert response.json()["Aleart"] == False
+
+    def main(self):
+        self.test_read_main()
+        self.test_read_co_ordinates()
+        self.test_api()
+        self.test_api_fun()
 if __name__ == "__main__":
+    unittest = unittest()
     unittest.main()
